c3e4fb143bae93706692c269ec5bc2c8acedd64d
Santhosh Datturi <dsanthosh.clg@gmail.com>
2025-02-01 21:53:37 +0530

    base changed to to git hub repo-name

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./", // Change this from '/' to './'
+  base: "/todoer/", // Change this from '/' to './'
 });
